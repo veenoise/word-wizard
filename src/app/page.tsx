c77e97f5440d7ee11ca8102ff2af570055015a32
veenoise <william.eduard123@gmail.com>
2025-07-02 23:04:08 +0800

    Fix: random word API changed hosting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
 
   const fetchRandomWord = async () => {
     setWaitingChallenge(true);
-    const getRandomWord = await fetch('https://random-word-api.herokuapp.com/word?length=5&lang=en');
+    const getRandomWord = await fetch('https://random-word-api.vercel.app/api?words=1&length=5');
     const randomWord:[string] = await getRandomWord.json();
     setChallengeWord(randomWord[0]);
     setWaitingChallenge(false);
